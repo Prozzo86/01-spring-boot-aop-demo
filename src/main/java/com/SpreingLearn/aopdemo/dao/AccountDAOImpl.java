8e88d86008b622e5c2b7d94d6b338cc2d44b0f6b
Darijan Bogdanović <dbogdanovic1@fina.hr>
2023-11-17 09:30:48 +0100

    Initial commit

---
+++ src/main/java/com/SpreingLearn/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,13 @@
+package com.SpreingLearn.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+// annotation @Repository for component scanning
+@Repository
+public class AccountDAOImpl implements AccountDAO{
+
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+    }
+}
